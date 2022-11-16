c56bab5dcef17bc7baebce7cac206d22fb30946f
waseem <waseem_6957@yahoo.com>
2022-11-16 20:40:11 +0530

    APIs completed. Web Pages Created. Integrated for single citizen and list of citizen.

--- src/main/java/com/learn/sampleapp/controller/UserController.java
+++ src/main/java/com/learn/sampleapp/controller/UserController.java
@@ -4,33 +4,20 @@ import com.learn.sampleapp.model.User;
 import com.learn.sampleapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
 
     @RequestMapping("/")
-    public List<User> getAllUser() {
-
-        return userService.getAllUsers();
-    }
-//    @RequestMapping("/")
-//    public String home() {
-//        return "home";
-//    }
-
-    @RequestMapping("/{id}")
-    public User getUser(@PathVariable int id) {
-        return userService.getUser(id);
-    }
-
-    @RequestMapping(value = "/add-user", method = RequestMethod.POST)
-    public void addUser(@RequestBody String username, @RequestBody String city) {
-//        User userRecord = new User(username, city);
-//        userService.addUser(userRecord);
+    public String home(ModelMap modelMap) {
+        modelMap.addAttribute("pagetitle","Login");
+        return "login";
     }
 }
+
--- src/main/webapp/views/login.jsp
+++ src/main/webapp/views/login.jsp
@@ -1,9 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ page isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Add Citizen</title>
+	<title>${pagetitle}</title>
 	<style type="text/css">
 		html,body{
 			margin: 0;
@@ -36,7 +41,7 @@
 				<tbody>
 					<tr>
 						<td>
-							<label for="username">Username</label>
+							<label for="user_name">Username</label>
 						</td>
 						<td>
 							<input type="text" id="user_name" class="input" name="user_name">
