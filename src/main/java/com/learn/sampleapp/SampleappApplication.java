491344eef5d14f6191b90f11d729ee661793c35c
waseem <waseem_6957@yahoo.com>
2022-11-17 17:56:57 +0530

    APIs completed. Web Pages Created. Integrated for single citizen and list of citizen.

--- src/main/java/com/learn/sampleapp/SampleappApplication.java
+++ src/main/java/com/learn/sampleapp/SampleappApplication.java
@@ -18,7 +18,7 @@ public class SampleappApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SampleappApplication.class, args);
-        CenterRepository centerRepository = context.getBean(CenterRepository.class);
+//        CenterRepository centerRepository = context.getBean(CenterRepository.class);
 
 //        VaccinationCenter vc1 = new VaccinationCenter();
 //        vc1.setCenterCity("Bengaluru");
@@ -34,37 +34,6 @@ public class SampleappApplication {
 //        vc3.setCenterCity("Pune");
 //        vc3.setCenterName("Central");
 //        centerRepository.save(vc3);
-//
-//        Citizen citizen = new Citizen();
-//        citizen.setVaccinationCenter(vc1);
-//        citizen.setCitizenCity("Mumbai");
-//        citizen.setCitizenName("Alex");
-//        citizen.setCitizenDoses(0);
-//        CitizenRepository citizenRepository = context.getBean(CitizenRepository.class);
-//        citizenRepository.save(citizen);
-
-//        System.out.println(c);
-
-        /*Optional<VaccinationCenter> data = centerRepository.findById(1);
-        System.out.println(data.get());
-        UserRepository userRepository = context.getBean(UserRepository.class);
-        User user = new User();
-        user.setUser_name("Waseem Attar");
-        user.setUser_email("waseem@gmail.com");
-
-        User u1 = userRepository.save(user);
-        System.out.println(u1);
-
-        Optional<User> optionalUser = userRepository.findById(1);
-        System.out.println(optionalUser.get());
-
-        Citizen c1 = new Citizen();
-        c1.setCitizenName("Alex");
-        c1.setCitizenCity("Mumbai");
-        c1.setCenterId(1);
-        c1.setCitizenDoses(0);
-        CitizenRepository citizenRepository = context.getBean(CitizenRepository.class);
-        citizenRepository.save(c1);*/
     }
 
 }
